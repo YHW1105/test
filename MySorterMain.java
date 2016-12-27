5b16fd084cffb038e663ae50717988640ad01443
YHW1105 <a19921105@hotmail.com>
2016-12-27 21:39:15 +0800

    new

--- MySorterMain.java
+++ MySorterMain.java
@@ -1,10 +1,13 @@
 package ntou.cs.sorter;
 
 public class MySorterMain {
+    public static void main(String[] args) {
+        double input[] = { 100, 10.10, 2.1, 2.0, 1.1, 1.0 };
+        MySorter sorter = new MySorter();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        sorter.sort(input);
+        for (double i: input) {
+            System.out.print(i + " ");
+        }
+    }
 }
